0f024e1adf8048f1fbff784ba06d621ec7b4582e
Domenicopezzullo <pezzullodomenico712@gmail.com>
2025-01-06 22:16:31 +0100

    Actually added the logic to send a message when the day comes

--- src/events/ready/ready.ts
+++ src/events/ready/ready.ts
@@ -1,5 +1,8 @@
 import type { Client } from 'discord.js';
+import { check_birthdays } from '../..';
 
 export default (client: Client<true>) => {
   console.log(`${client.user.tag} is online!`);
+  check_birthdays()
+  setInterval(check_birthdays, 360000);
 };
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import 'dotenv/config';
 
-import { Client, IntentsBitField } from 'discord.js';
+
+import { Client, IntentsBitField, TextChannel } from 'discord.js';
 import { CommandKit } from 'commandkit';
 import { join, dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import mongoose from 'mongoose';
-
+import { ChannelModel } from './schemas/Channel';
+import { BirthdayModel } from './schemas/Birthday';
 
 if (process.env.MONGODB_URI) {
   mongoose.connect(process.env.MONGODB_URI)
@@ -29,5 +31,32 @@ new CommandKit({
   eventsPath: join(__dirname, 'events'),
   commandsPath: join(__dirname, 'commands'),
 });
+
+export async function check_birthdays() {
+  let monthnumber = new Date().getUTCMonth()+1
+  let monthstring = monthnumber.toString().padStart(2, '0')
+  let day = new Date().getUTCDate().toString().padStart(2, '0')
+  let date = `${day}/${monthstring}`
+  
+  let birthdays = await BirthdayModel.find({birthday: date})
+  
+  birthdays.forEach(async (birthday) => {
+    const channelToFetch = await ChannelModel.findOne({ guildId: birthday.guildId });
+  
+    if (channelToFetch) {
+      if (!channelToFetch.channelId) return
+  
+      const channelId = channelToFetch.channelId.toString();
+      const guild = await client.guilds.fetch(birthday.guildId);
+      const user = await guild.members.fetch(birthday.userId);
+      const channelToSend = await guild.channels.fetch(channelId) as TextChannel;
+  
+      if (channelToSend) {
+        channelToSend.send(`Happy Birthday ${user.displayName}!`);
+      }
+    }
+  }); 
+}
+
 
 client.login(process.env.TOKEN);
